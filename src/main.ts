f012b28b2778d04febb0121706b9f7021fb7a244
Laurie Trichet <laurie.trichet@gmail.com>
2019-07-23 20:51:12 +1000

    add the type to add images as modules

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+/// <reference path="./image.d.ts" />
 import Greetings from "./greets";
-
+import panda from './assets/images/panda.jpg';
 console.log(new Greetings().sayHello("TypeScript"));
+const home = document.getElementById("home") as HTMLImageElement;
+home.src = panda;
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,52 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
-  entry: './src/main.ts',
-  mode:'development',
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [ // compile ts files to js that run on the browser
-    {
-       test: /\.(js|jsx|tsx|ts)$/,
-       exclude: /node_modules/,
-       loader: 'babel-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
-  },
-  plugins: [
-    new CopyPlugin([ // copy the html file over to the dist folder
-      { from: 'index.html', to: path.resolve(__dirname, 'dist') },
-    ]),
-  ],
+    entry: './src/main.ts',
+    mode: 'development',
+    devtool: 'inline-source-map',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [ // compile ts files to js that run on the browser
+            {
+                test: /\.(js|jsx|tsx|ts)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'assets',
+                            name: '[folder][name].[ext]'
+                        },
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: 'assets',
+                            name: '[folder][name].[ext]'
+                        },
+                    }
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    plugins: [
+        new CopyPlugin([ // copy the html file over to the dist folder
+            { from: 'index.html', to: path.resolve(__dirname, 'dist') },
+        ]),
+    ],
 };
