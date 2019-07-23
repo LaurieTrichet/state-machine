2b05a18996541210e4008a2ba0a6ab98b52cf6dc
Laurie Trichet <laurie.trichet@gmail.com>
2019-07-23 22:46:20 +1000

    fix app when running on node

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@
 import Greetings from "./greets";
 import panda from './assets/images/panda.jpg';
 console.log(new Greetings().sayHello("TypeScript"));
-const home = document.getElementById("home") as HTMLImageElement;
-home.src = panda;
+if (typeof document !== 'undefined'){
+    const home = document.getElementById("home") as HTMLImageElement;
+    home.src = panda;
+}
